d9b8c32ee45c322b8f3330e90d40dbacd14be779
Achmad Qizwini <akise.shun@gmail.com>
2024-02-25 23:48:00 +0700

    add some routing

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+
+from sqlalchemy.orm import Session
+import os
 
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
+
+from .model.user import user
+from .schemas.user import UserResponse
+from .database.settings import SessionLocal, engine
+from .database.connection import get_db
+from .controller import get_item
+
+user.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
@@ -21,3 +34,10 @@ app.include_router(
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
+@app.get("/userrr", response_model=UserResponse)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    item = get_item(db, item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
